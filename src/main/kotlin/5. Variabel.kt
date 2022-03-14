ac7334ca16992cbc228904afeb53b9926deda558
Asus <imamsetiawan27062000@gmail.com>
2022-03-14 14:22:26 +0700

    Variabel

---
+++ src/main/kotlin/5. Variabel.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    //tipe data variabel dinamis
+    var dynamicVar = 2
+    println("contoh dynamic variabel = " + dynamicVar)
+
+    //tipe data variabel statis
+    var staticVar = 'A'
+    println("contoh statis Variabel = " + staticVar)
+
+}
