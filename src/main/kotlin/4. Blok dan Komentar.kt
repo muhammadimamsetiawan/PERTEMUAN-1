fc9f0d789ef917c16148d54998cd03fbbf54c044
Asus <imamsetiawan27062000@gmail.com>
2022-03-14 14:19:52 +0700

    Blok dan Komentar

---
+++ src/main/kotlin/4. Blok dan Komentar.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    //Contoh block, block ditandai dengan buka dan tutup kurawal
+    println("Hello, ini program pertama saya di kotlin!")
+
+    //contoh komentar, comment adalah baris komenter pada program program yang tidak akan dieksekusi oleh compiler
+    //contoh komentar satu baris
+    /*
+    contoh komentar multi baris
+     */
+
+    /**
+     * kotlin Doc --> baris ini untuk mendokumentasikan  kode program
+     */
+}
